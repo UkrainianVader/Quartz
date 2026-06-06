d5e67a24dfec7d140f84ab3a65b74641bf5b9dac
UkrainianVader <symphonicnightcore@gmail.com>
2026-06-06 19:20:06 +0300

    OOPS

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,24 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, APP_INITIALIZER } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { AuthService } from './auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withFetch()),
-    provideClientHydration(withEventReplay()),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: (authService: AuthService) => {
-        return () => authService.initialize().toPromise();
-      },
-      deps: [AuthService],
-      multi: true
-    }
+    provideClientHydration(withEventReplay())
   ]
 };
